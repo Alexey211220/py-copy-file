604557218f75d4fbf48607bc4fd13e0edbb0bc0b
Alexey <alexey.zhuravel2004@gmail.com>
2026-02-21 00:35:19 +0100

    Used descriptive and correct variable names

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) == 3 and parts[0] == "cp":
+        old_file, new_file = parts[1], parts[2]
         if parts[1] != parts[2]:
             try:
-                with open(parts[1], "r") as new, open(parts[2], "w") as old:
+                with open(old_file, "r") as new, open(new_file, "w") as old:
                     for line in new:
                         old.write(line)
             except (FileNotFoundError):
