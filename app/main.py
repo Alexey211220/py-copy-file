63c7432bcc9bd9cd0afc4c4ad15410117ca25a56
Alexey <alexey.zhuravel2004@gmail.com>
2026-02-21 00:27:41 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    parts = command.split()
+
+    if len(parts) == 3 and parts[0] == "cp":
+        if parts[1] != parts[2]:
+            try:
+                with open(parts[1], "r") as new, open(parts[2], "w") as old:
+                    for line in new:
+                        old.write(line)
+            except (FileNotFoundError):
+                pass
